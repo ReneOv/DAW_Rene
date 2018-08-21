3dc804533dde872095c6f5aa9c9d378bafaf9082
ReneOv <36871265+ReneOv@users.noreply.github.com>
2018-08-20 20:59:33 -0500

    Entrega Lab 4

---
+++ Lab04/acm.js
@@ -0,0 +1,16 @@
+//Ejercicio 6
+
+document.write("<br>Ejemplo 1:<br>INPUT" +
+    "<br>3" +
+    "<br>17 18" +
+    "<br>15 24" +
+    "<br>12 15" +
+    "<br>OUTPUT" +
+    "<br>6");
+
+document.write("<br><br>Ejemplo 2:<br>INPUT" +
+    "<br>2" +
+    "<br>10 16" +
+    "<br>7  17" +
+    "<br>OUTPUT" +
+    "<br>-1");
